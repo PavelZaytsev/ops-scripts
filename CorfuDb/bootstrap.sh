0d8a48521ed1a44d29703bc0e7d70d46a2d7e348
Pavel Zaytsev <pzaytsev@pzaytsev-a01.vmware.com>
2019-05-10 08:28:36 -0700

    demo

--- CorfuDb/bootstrap.sh
+++ CorfuDb/bootstrap.sh
@@ -1,9 +1,25 @@
 #!/usr/bin/env bash
 
-kubectl apply -f es/elasticsearch.yaml # elasticsearch
-helm install stable/prometheus --name prometheus -f prometheus/values.yaml # prometheus
-helm install --name jaeger stable/jaeger-operator # jaeger operator
-kubectl apply -f jaeger/jaeger.yaml # jaeger
-kubectl apply -f kibana/kibana.yaml # kibana
-kubectl apply -f fluent-bit/fluent-bit-daemonset-elasticsearch.yaml # fluent-bit
-kubectl apply -f jmx-exporter/exporter.yaml # corfu metrics prometheus exporter
+set -e -x
+
+if [ $1 = "up" ]; then
+  kubectl apply -f es/elasticsearch.yaml # elasticsearch
+  helm install stable/prometheus --name prometheus -f prometheus/values.yaml # prometheus
+  helm install --name jaeger stable/jaeger-operator # jaeger operator
+  kubectl apply -f jaeger/jaeger.yaml # jaeger
+  kubectl apply -f kibana/kibana.yaml # kibana
+  kubectl apply -f fluent-bit/fluent-bit-daemonset-elasticsearch.yaml # fluent-bit
+  kubectl apply -f jmx-exporter/exporter.yaml # corfu metrics prometheus exporter
+elif [ $1 = "down" ]; then
+  kubectl delete -f es/elasticsearch.yaml # elasticsearch
+  helm delete prometheus --purge # prometheus
+  helm delete jaeger --purge # jaeger-operator
+  kubectl delete -f jaeger/jaeger.yaml # jaeger
+  kubectl delete -f kibana/kibana.yaml # kibana
+  kubectl delete -f fluent-bit/fluent-bit-daemonset-elasticsearch.yaml # fluent-bit
+  kubectl delete -f jmx-exporter/exporter.yaml # corfu metrics prometheus exporter
+  kubectl delete -f corfu/corfu.yaml # corfu
+else
+  echo "Unknown command"
+  exit 1
+fi
--- CorfuDb/connect.sh
+++ CorfuDb/connect.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 set -e -x
 
-kill $(lsof -t -i :8001)
-kill $(lsof -t -i :5601)
-kill $(lsof -t -i :16686)
-kubectl proxy &
-kubectl port-forward $(kubectl get pods --no-headers -o=name -l k8s-app=kibana-logging) 5601 &
-kubectl port-forward $(kubectl get pods --no-headers -o=name -l app=prometheus) 9090 &
-kubectl port-forward $(kubectl get pods --no-headers -o=name -l app=jaeger) 16686 &
+if [ $1 = "kill" ]; then
+  kill $(lsof -t -i :8001)
+  kill $(lsof -t -i :5601)
+  kill $(lsof -t -i :9090)
+  kill $(lsof -t -i :16686)
+elif [ $1 = "forward" ]; then
+  kubectl proxy &
+  kubectl port-forward $(kubectl get pods --no-headers -o=name -l k8s-app=kibana-logging) 5601 &
+  kubectl port-forward $(kubectl get pods --no-headers -o=name -l app=prometheus) 9090 &
+  kubectl port-forward $(kubectl get pods --no-headers -o=name -l app=jaeger | grep jaeger-query) 16686 &
+else
+  echo "Unknown command"
+  exit 1
+fi
