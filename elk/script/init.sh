159056fe579856617c9d1fbfb63af34c574d6ca2
Viacheslav <xrw.null@gmail.com>
2019-06-17 23:16:47 -0700

    Docker-elk refactoring. Initial step, execute: ./script/init.sh

---
+++ elk/script/init.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+#download docker-elk repo into build directory
+rm -rf build
+git clone https://github.com/deviantony/docker-elk.git build/docker-elk
+
+#copy configs into build dir
+cp -R src/logstash build/docker-elk
+cp -R src/docker-compose.yml build/docker-elk
