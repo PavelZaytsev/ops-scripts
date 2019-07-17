9330007b9655564a6646633c024b1bd7c144a370
Viacheslav <xrw.null@gmail.com>
2019-07-17 11:26:20 -0700

    Corfu logs Post processing

--- elk/support-bundle-tools.sh
+++ elk/support-bundle-tools.sh
@@ -6,3 +6,9 @@ docker run -ti \
 -v $(pwd)/build/docker-elk/data:/output/data/ \
 --workdir=/bug \
 pypy:3-7.0 sh -c "/usr/local/bin/pypy3 /prepare_logs.py $1"
+
+cd build/docker-elk/data
+
+find . -type f -name "corfu.9000*.log" -print0 | xargs -0 sed -i '' -e '/Trying connection to layout server/d'
+find . -type f -name "corfu.9000*.log" -print0 | xargs -0 sed -i '' -e '/log write/d'
+find . -type f -name "corfu.9000*.log" -print0 | xargs -0 sed -i '' -e '/New tail segment less than or equals to the old one/d'
